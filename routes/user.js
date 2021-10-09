0ea5ef77746c5a4c1fed59ce145d8a99083e77e4
Joel D <imaginethere.snoie@gmail.com>
2021-10-09 11:52:14 +0300

    Make post register and login routes

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 
 const pool = require('../helpers/database');
 
@@ -14,8 +15,42 @@ router.get('/:id', async (req, res, next) => {
   } catch (err) {
     res.status(500).send(err.message);
   }
+});
 
-  res.status(200).json({id: req.params.id});
+router.post('/register', async (req, res, next) => {
+  try {
+    const {email, password} = req.body;
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const sqlQuery = 'INSERT INTO user (email, password) VALUES (?,?)';
+    const result = await pool.query(sqlQuery, [email, encryptedPassword]);
+
+    res.status(200).json(result);
+
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const {id, enteredPassword} = req.body;
+
+    const sqlGetUser = 'SELECT password FROM user WHERE id=?';
+    const rows = await pool.query(sqlGetUser, id);
+
+    if (rows) {
+      const isValid = await bcrypt.compare(enteredPassword, rows[0].password);
+
+      return res.status(200).json({isPasswordValid: isValid});
+    }
+
+    res.status(200).send(`User with id ${id} was not found`);
+
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
 module.exports = router;
